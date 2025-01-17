d5372d66f2fde16805602751d71ea6fc68a742ac
ChrisSchinnerl <3903476+ChrisSchinnerl@users.noreply.github.com>
2025-01-17 06:42:12 +0000

    hostd: v2.0.2

--- Formula/hostd.rb
+++ Formula/hostd.rb
@@ -1,7 +1,7 @@
 class Hostd < Formula
   desc "Hosting daemon for the Sia network"
   homepage "https://github.com/SiaFoundation/hostd"
-  url "https://github.com/SiaFoundation/hostd.git", tag: "v2.0.1"
+  url "https://github.com/SiaFoundation/hostd.git", tag: "v2.0.2"
 
   depends_on "gcc" => :build
   depends_on "go" => :build
