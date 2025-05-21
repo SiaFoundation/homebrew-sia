69729842467ee4b66beb5b0a9a55b2d89b3b6151
ChrisSchinnerl <3903476+ChrisSchinnerl@users.noreply.github.com>
2025-05-21 07:37:10 +0000

    walletd: v2.6.0

--- Formula/walletd.rb
+++ Formula/walletd.rb
@@ -1,7 +1,7 @@
 class Walletd < Formula
   desc "Dedicated Sia wallet"
   homepage "https://github.com/SiaFoundation/walletd"
-  url "https://github.com/SiaFoundation/walletd.git", tag: "v2.5.0"
+  url "https://github.com/SiaFoundation/walletd.git", tag: "v2.6.0"
 
   depends_on "gcc" => :build
   depends_on "go" => :build
