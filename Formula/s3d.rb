5dc260fe18ed0b25990257ffb10deaec90b5ae25
n8mgr <5544150+n8mgr@users.noreply.github.com>
2026-09-11 09:22:54 +0000

    s3d: v0.1.4

--- Formula/s3d.rb
+++ Formula/s3d.rb
@@ -1,7 +1,7 @@
 class S3d < Formula
   desc "Lightweight, S3-compatible Renter for the Sia network"
   homepage "https://github.com/SiaFoundation/s3d"
-  url "https://github.com/SiaFoundation/s3d.git", tag: "v0.1.3"
+  url "https://github.com/SiaFoundation/s3d.git", tag: "v0.1.4"
 
   depends_on "gcc" => :build
   depends_on "go" => :build
